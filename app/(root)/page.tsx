493a285f42d39136f0ea7e98f83e4cf76aa07bd2
Uchenna Oko <uche4newton1981@gmail.com>
2026-09-25 19:15:32 +0100

    Implement Fetch Handler

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import handleError from "@/lib/handlers/error";
 import { ValidationError } from "@/lib/http-errors";
+import { api } from "@/lib/api";
 
 const questions = [
   {
@@ -51,10 +52,7 @@ const questions = [
 
 const test = async () => {
   try {
-    throw new ValidationError({
-      title: ["Required"],
-      tags: ['"JavaScript" is not a valid tag.'],
-    });
+    return await api.users.getAll();
   } catch (error) {
     return handleError(error);
   }
@@ -65,7 +63,9 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  await test();
+  const users = await test();
+
+  console.log(users)
 
   const { query = "", filter = "" } = await searchParams;
 
--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -5,11 +5,14 @@ import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import { AccountSchema } from "@/lib/validations";
+import dbConnect from "@/lib/mongoose";
 
 export async function POST(request: Request) {
   const { providerAccountId } = await request.json();
 
   try {
+    await dbConnect();
+
     const validatedData = AccountSchema.partial().safeParse({
       providerAccountId,
     });
