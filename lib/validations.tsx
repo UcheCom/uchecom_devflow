ae1cc249839dec1fe7240d3719b7b8c92ae65ebf
Uchenna Oko <uche4newton1981@gmail.com>
2026-09-25 05:43:46 +0100

    Implement Create & Read Account Routes

--- lib/validations.tsx
+++ lib/validations.tsx
@@ -1,42 +1,47 @@
 import { z } from "zod";
 
 export const SignInSchema = z.object({
-  email: z.email({ message: "Please enter a valid email address" }).min(1, { message: 'Email is required.' }),
-    
+  email: z.email({ message: "Please provide a valid email address." }).min(1, { message: "Email is required" }),
+
   password: z
     .string()
-    .min(6, { message: "Password must be at least 6 characters" })
-    .max(100, { message: "Password must not exceed 100 characters" }),
+    .min(6, { message: "Password must be at least 6 characters long. " })
+    .max(100, { message: "Password cannot exceed 100 characters." }),
 });
 
 export const SignUpSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'Username must be at least 3 characters long.' })
-    .max(30, { message: 'Username cannot exceed 30 characters.' })
+    .min(3, { message: "Username must be at least 3 characters long." })
+    .max(30, { message: "Username cannot exceed 30 characters." })
     .regex(/^[a-zA-Z0-9_]+$/, {
-      message: 'Username can only contain letters, numbers, and underscores.',
+      message: "Username can only contain letters, numbers, and underscores.",
     }),
 
   name: z
     .string()
-    .min(1, { message: 'Name is required.' })
-    .max(50, { message: 'Name cannot exceed 50 characters.' })
+    .min(1, { message: "Name is required." })
+    .max(50, { message: "Name cannot exceed 50 characters." })
     .regex(/^[a-zA-Z\s]+$/, {
-      message: 'Name can only contain letters and spaces.',
+      message: "Name can only contain letters and spaces.",
     }),
 
-  email: z
-    .email({ message: 'Please provide a valid email address.' })
-    .min(1, { message: 'Email is required.' }),
+  email: z.email({ message: "Please provide a valid email address." }).min(1, { message: "Email is required." }),
 
   password: z
     .string()
-    .min(6, { message: 'Password must be at least 6 characters long.' })
-    .max(100, { message: 'Password cannot exceed 100 characters.' })
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .max(100, { message: "Password cannot exceed 100 characters." })
     .regex(/[A-Z]/, {
-      message: 'Password must contain at least one uppercase letter.',
+      message: "Password must contain at least one uppercase letter.",
     })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character.",
+    }),
 });
 
 export const AskQuestionSchema = z.object({
@@ -61,4 +66,27 @@ export const UserSchema = z.object({
   location: z.string().optional(),
   portfolio: z.url({ message: "Please provide a valid URL." }).optional(),
   reputation: z.number().optional(),
+});
+
+export const AccountSchema = z.object({
+  userId: z.string().min(1, { message: "User ID is required." }),
+  name: z.string().min(1, { message: "Name is required." }),
+  image: z.url({ message: "Please provide a valid URL." }).optional(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .max(100, { message: "Password cannot exceed 100 characters." })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter.",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character.",
+    })
+    .optional(),
+  provider: z.string().min(1, { message: "Provider is required." }),
+  providerAccountId: z.string().min(1, { message: "Provider Account ID is required." }),
 });
